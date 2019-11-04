10fd3a8cd5dda1e781f4a03a032fc2f17dcc1681
Jim Williams <james.8.williams@gsk.com>
2019-11-04 10:30:48 -0500

    Latest

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+'use strict';
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  request = require('request'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening on port 1337'));
+
+/************************************************************/
+/* POST from Google Assistant                               */
+/************************************************************/
 app.post('/', (req, res) => { 
   ts("index:post","POST from Google Asssistant"); 
   res.setHeader('Content-Type', 'application/json');
   res.append('Google-Assistant-API-Version', 'v2');
   res.json(getResponse());
-}); 
+});  
 
+/************************************************************/
+/* Healthcheck endpoint                                     */
+/************************************************************/
+app.get('/health', (req, res) => {
+  ts("index:post","Health is good"); 
+  res.status(200).send("Health is good");
+});
 
-
-
+/************************************************************/
+/* getResponse                                              */
+/************************************************************/
 function getResponse() {
   var returnValue = {
     "expectUserResponse": true,
@@ -35,6 +58,13 @@ function getResponse() {
   return returnValue;
 }
 
+/************************************************************/
+/* ts - logging function                                    */
+/************************************************************/
+function ts(location, message)
+{
+   console.log("[" + new Date().toISOString() + "]" + " " + location + " ==> " + message);
+}
 
 
 
